a24a2c0162ebae3d26e137c20c8d203ff9ff2896
username2512 <oniemmanueljesufemi@gmail.com>
2022-08-13 09:48:49 +0100

    initial commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestAreStateful::class,
+           // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestAreStateful::class,
         ],
 
         'api' => [
